bc09a2303fc54f51ecb64c686eee6b2dced82e2b
vathin <134401419+vathin@users.noreply.github.com>
2023-05-27 20:09:59 +0300

    Add files via upload
    v 0.9:
    -Добавлено главное меню
    -Добавлен выбор уровней
    -Добавлен второй уровень
    -Добавлен спидометр

---
+++ race_game v0.9/race_game_levels.py
@@ -0,0 +1,92 @@
+level1 = ['--------------------------------------------------------------------------------------------ggggggg',
+         '- ==                                 c                             ==                      ---pggggg',
+         '- =                                                                                          =--gpgg',
+         '-                                                                                               -ggg',
+         '-=                                                                                               -gg',
+         '-==                                                                             c                -wg',
+         '---------              --------------------------------------------------------------  c          -g',
+         'gfgggw-             ----     ---wgggggggwggggggggggg-------          ------ggggpgggwg---b         -g',
+         'fgpgg-            --==         --gggggggggggffgggg--          c    c       ---gggggggggg--      c -g',
+         'gggg-            -               -gggpgggggfgfggg-       c                    ---ggggggggg-       b-',
+         'gggg-           -                 -ggggggggpgggg-   c                            --gggggggg-       -',
+         'ggwg-         --c          c       -gggggggggggg-                  ==              -ggggggw-      c-',
+         'ggg-         -g-          --        -ggggggggggg-             ------------          -ggggggg-      -',
+         'wgg-  c  c   -g-       =--gp-        -gpgggggggg-           --gwgggggggggg-          -gggggg-      -',
+         'gg-         -g-        -ggggp-        -gggggggg-b          -ggggggggggggwgg-          ---gg-       -',
+         'gg-         -g-       =-ggggg-         --gggg--           -ggpggggggggggggg-             --        -',
+         'gg-         -g-       -gpggggg-          ----==           -ggggggggggggggggg-                      -',
+         'g-         -gg-       -gggpfgg-                          b-gggggggggggggggpg-                     --',
+         'w-========-pgg-       -ggggffgg-                         -gggggggggpggggggggg-                   -g-',
+         'g-        -gpg-       -ggggggggg-                        -gggggggggggggggggggg--=            == -pg-',
+         'g-=       -ggg-        -ggggggggg--                     -gggggggggggggggggggpggg--- b        ==-ggg-',
+         'g-=        -gg-        -gggggggggwg----        ==  -----ggggwgggggggggggggggggggggg-----------------',
+         'gg-        -gg-         -ggggggggggpggg------------gggpggggggg------------------------gggggggggwgggg',
+         'gg-        -ggg-         -------------------------------------==                    ==----gggggggggg',
+         'gp-         -gg-                        bb                    ==                     =    ---ggpgggg',
+         'gg-         -ggg-b==                                                                         --ggggg',
+         'ggg-         -fgg--=                                                                           -gggg',
+         'ggg-          -fggg---                                       b --------------------             -ggg',
+         'ggg-           -ffgggg-----------------------------------------gggggpgggggggggggggg---          -ggg',
+         'pggg-           ---gfgggggggwggggggggggggpggggg-----==        b--------ggggggggggggggg-          -gg',
+         'gggg-              ---fpggggggggggggggggggg----     =                  ---ggggggggggggg-         -gg',
+         'ggggg-                ---gpggggffgggggggg--=                              -gggggwgggggg-         -gg',
+         'gggggg--                 ---ggggfgggggg--                b                 --ggggggggg-         -ggg',
+         'gggggpgg--                  ---gggggg--              --------                ---gggg--  c    c  -ggg',
+         'gggggggggg---                  ---gg- c          ----gggggggg---                ----            -ggg',
+         'ggggggggggpgg---                 =--      c   ---gggggggggpggggg--               =             -gwgg',
+         'ggggggggggggggpg---                         --gggggggggggggggggggg-                            -gggg',
+         'ggggggggggggggggggg---                     -gwggggggggggggggggggggg-                         =-ggpgg',
+         'ggggggpggggggggggggggg---                 -ggggggggggggggpgggggggwgg--                      --gggggg',
+         'ggggggggggggggfgffggggggg---            --gpgggggggggggggggggggggggggg---=    c   c      ---gggggggg',
+         'gggggggggggggggffgggggggpggg----     ---ggggggggggggggggggggggggggggggggg----        ----ggggggggggg',
+         'gggggggggggggggggggggggggggggggg-----gggggggggggggggggggggggggggggggggggggggg--------ggggggggggggggg',
+         'gggggggggggggggggggggggggggggggggggggggggggggggggggggpggggggggggggggggggggggggggggggpggggggggggggggg']
+
+
+level2 = ['----------------------------------------------------------------------------------',#1
+          '-fpggggfggg--                                                        --fpggggfggg-',#2
+          '-fggfgggpg-    c         c         c         c         c         c     -fggfgggpg-',#3
+          '-gggggggg-                                                              -gggggggg-',#4
+          '-ggfgggg=                                                                =ggfgggg-',#5
+          '-fgpggg=                                                                  =fgpggg-',#6
+          '-ggfgg=     c  cdddddddddddddddddddc          cdddddddddddddddddddc        =ggfgg-',#7
+          '-ggfg=        =-ffffffffffffffffpff-          -ffwfffffffffffffffw-=        =ggfg-',#8
+          '-gfg=         ----------------------b        b----------------------         =gfg-',#9
+          '-fg=         rg-gwggggpggggggggpgggl          rggwgggggggpgffggpgg-gl         =fg-',#10
+          '-g-          rg-gwpfgffgggfgffggggflc        crpggffgfgggfgggppggg-gl          -g-',#11
+          '--           rf-ggggggg---------fggl          rffg---------wpfgffg-fl           --',#12
+          '--           rf-gfgggpg-=pggg==-ggpl          rfwg-=gggg==-ggggggg-fl           --',#13
+          '-            rp-gwggggg-wggpgp=-fggl          rgfg-=ggfgp=-pgggffg-fl            -',#14
+          '-            rg-ggggfgg-ggggggp-ggfl          rfgf-ggggggg-gggggff-pl            -',#15
+          '-            rg-fggfgpg-fpggfgg-ffgl          rggg-gfgfggw-ggpfggf-gl            -',#16
+          '-            rw-ffggggg-ggfggfg-ggwl          rpgg-pgfgfgf-gggffgp-gl            -',#17
+          '-            rg-fgggpgf-gfgffgp-gggl          rgwg-fgffgfg-gwggggf-fl            -',#18
+          '-            rg-fwggffg-fgfgffg-gffl          rggg-ffpffgf-gfgfggf-gl            -',#19
+          '-            rf-gfggpfg-ffffpff-ffgl          rfgg-ffffffw-ggfggff-gl            -',#20
+          '-             u=fgggffg-fffffff-pgfl          rgfg-fffffff-ffgfpfw=u             -',#21
+          '-               uuuuuuu=uuuuuuu=uuu c        c uuu=uuuuuuu=uuuuuuu               -',#22
+          '-                                                                                -',#23
+          '-                                                                                -',#24
+          '-               ddddddd=ddddddd=ddd c        c ddd=ddddddd=ddddddd               -',#25
+          '-             d=gwpfgfg-fffffff-ffgl          rfgf-fffffff-gggffgg=d             -',#26
+          '-            rg-ggggggg-ffffffw-gfgl          rpff-ffffpff-fggpfgf-fl            -',#27
+          '-            rf-gpggffg-ffpffgf-fggl          rggf-fgfgffg-wggffgf-gl            -',#28
+          '-            rf-fgggggf-fgffgfg-gwgl          rfgf-gfgffgp-gggpgfg-gl            -',#29
+          '-            rp-fggpfgg-pgfgfgf-pggl          rfff-ggfggfg-fgggggf-fl            -',#30
+          '-            rf-pgggffg-gfgfggw-ggglc        crggg-fpggfgg-ggfgpgf-pl            -',#31
+          '-            rg-fgwgggg-ggggggg-fgfl          rpfg-ggggggp-gggfggp-gl            -',#32
+          '-            rg-fgfgfgg-=ggfgp=-gfgl          rggg-wggpgp=-wgggggg-gl            -',#33
+          '--           rg-gggfggf-=gggg==-fwgl          rggg-=pggg==-gfgggpf-fl           --',#34
+          '--           rf-pffgfpf---------ffgl          rfgg---------ggggggf-fl           --',#35
+          '-g-          rf-gpggffgfgggfgggppggl          rggggpggfgggfggpgggg-gl          -g-',#36
+          '-fg=         rg-gfgfgpfffggffgggfwfl=        =rgffpggfggfgpwffgggf-pl         =gg-',#37
+          '-gfw=         ----------------------b        b----------------------         =fgg-',#38
+          '-ggfg=        =-gggfffgfppfgggggfgf-          -gffgfggfpffgfpfgggg-         =ffgg-',#39
+          '-ggfgg=         uuuuuuuuuuuuuuuuuuu            uuuuuuuuuuuuuuuuuuu         =ggpgg-',#40
+          '-fgpggg=                                                                  =pggggg-',#41
+          '-gpfgggg=                                                                =ggggfgg-',#42
+          '-gggggggg-                                                              -gpfggggg-',#43
+          '-fggfgggpg-                                                            -ffgggpggf-',#44
+          '-fpgpggfggg--                                                        --fpggggfggg-',#45
+          '----------------------------------------------------------------------------------']#46
+
